2908e2b0675fe68f5b3218ad4ccf0eb20e4c5ee6
imahnama <muannama25@gmail.com>
2020-12-21 15:24:57 +0300

    Create models/controllers and set up rspec for testing

---
+++ db/migrate/20201221120721_create_measurements.rb
@@ -0,0 +1,11 @@
+class CreateMeasurements < ActiveRecord::Migration[6.0]
+  def change
+    create_table :measurements do |t|
+      t.numeric :duration
+      t.string :date
+      t.references :activity, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
