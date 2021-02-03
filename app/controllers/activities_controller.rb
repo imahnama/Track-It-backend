8354cd258f8c84b393e8688a348022ef1808e628
imahnama <muannama25@gmail.com>
2021-02-03 13:22:57 +0300

    disable Csrf token

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,4 +1,5 @@
 class ActivitiesController < ApplicationController
+  skip_before_action :verify_authenticity_token
   before_action :set_activity, only: %i[show update destroy]
 
   def index
--- app/controllers/measurements_controller.rb
+++ app/controllers/measurements_controller.rb
@@ -1,4 +1,5 @@
 class MeasurementsController < ApplicationController
+  skip_before_action :verify_authenticity_token
   before_action :set_activity
   before_action :set_activity_measurement, only: %i[show update destroy]
 
